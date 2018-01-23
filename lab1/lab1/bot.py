e09fb8045ecc29817b3a82560a2f8dadba464937
mlvm2 <vivraaj@gmail.com>
2018-01-23 12:40:54 +0000

    lab1

---
+++ lab1/lab1/bot.py
@@ -0,0 +1,13 @@
+from sopel import module
+from emo.wdemotions import EmotionDetector
+
+emo = EmotionDetector()
+
+@module.rule('')
+def hi(bot, trigger):
+    print(trigger, trigger.nick)
+    array = emo.detect_emotion_in_raw_np(trigger)
+    array = array.tolist()
+    array = [int(i) for i in array]
+    bot.say(str(array))
+    #bot.say('Hi, ' + trigger.nick)
